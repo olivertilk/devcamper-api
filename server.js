037caa25f9ad5803fcda0c60be534fc6c9e17dc9
Oliver Tilk <olivertilk@gmail.com>
2019-10-18 14:39:56 -0700

    Initial Express setup

---
+++ server.js
@@ -0,0 +1,12 @@
+//Learning about HTTP using the Node core HTTP module and not using Express just yet
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({path: './config/config.env'});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
